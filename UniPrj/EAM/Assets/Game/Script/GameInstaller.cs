7550ff9f9a47320d03e52a5b21fd48c162a06c37
fancyblock <xujia1985@gmail.com>
2021-09-08 00:31:06 +0800

    misc

--- UniPrj/EAM/Assets/Game/Script/GameInstaller.cs
+++ UniPrj/EAM/Assets/Game/Script/GameInstaller.cs
@@ -5,5 +5,21 @@ using Zenject;
 
 public class GameInstaller : MonoInstaller
 {
-    
+    public override void InstallBindings()
+    {
+        SignalBusInstaller.Install(Container);
+
+    //    Container.Bind(typeof(ITickable), typeof(IInitializable), typeof(GameController)).To<GameController>().AsSingle();
+
+#if UNITY_EDITOR
+        Container.Bind<IGameSettingLoader>().To<EditorGameSettingLoader>().AsSingle();
+#else
+        Container.Bind<IGameSettingLoader>().To<RuntimeGameSettingLoader>().AsSingle();
+#endif
+
+        // game config
+    //    Container.Bind<MapRoad>().FromMethodMultiple((InjectContext context) => { return context.Container.Resolve<IGameSettingLoader>().LoadData<MapRoad>("MapRoad"); });
+    //    Container.Bind<MapBuilding>().FromMethodMultiple((InjectContext context) => { return context.Container.Resolve<IGameSettingLoader>().LoadData<MapBuilding>("MapBuilding"); });
+
+    }
 }
