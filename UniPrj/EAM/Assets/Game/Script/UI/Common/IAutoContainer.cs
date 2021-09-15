b0ba5c8af1980e7663b506d28fb7491eafa6ee16
fancyblock <xujia1985@gmail.com>
2021-09-15 23:41:01 +0800

    add some ui common stuff

---
+++ UniPrj/EAM/Assets/Game/Script/UI/Common/IAutoContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public interface IAutoContainer  
+{
+    void SetEnable(bool enable);
+    int LAST_BTN_FRAME { get; set; }
+
+    IAutoWidget GetWidget(string name);
+    T GetWidget<T>(string name);
+
+    void SetEventEnable(bool enable);
+
+    bool IsVirtual(string widgetName);
+    void SetEnable(string widgetName, bool enable);
+    void SetValue(string widgetName, string text);
+    void SetValue(string widgetName, bool toggle);
+    void SetValue(string widgetName, float progress);
+    void SetButtonEvent(string widgetName, Action evt);
+    void SetButtonEnable(string widgetName, bool enable);
+    void SetImageFillAmount(string widgetName, float value);
+    float GetImageFillAmount(string widgetName);
+    void PlayAnimation(string widgetName, string aniName);
+
+    void SetButtonClkHook(Action<string> hook);
+
+    GameObject GetExObject(int index);
+}
---
+++ UniPrj/EAM/Assets/Game/Script/UI/Common/IAutoWidget.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAutoWidget 
+{
+    bool IsVirtual();
+
+    void SetEnable(bool enable);
+
+    GameObject GetGameObj();
+
+    T Component<T>();
+
+    void SetValue(string text);
+
+    void SetValue(bool toggle);
+
+    void SetValue(float progress);
+
+    void SetValue(Sprite sprite);
+
+    void SetButtonEvent(Action evt);
+
+    void SetButtonEvent(Action<string> evt);
+
+    void SetTriggerEvent(Action evt);
+
+    void SetButtonEnable(bool enable);
+
+    void SetImageFillAmount(float value);
+
+    void SetTextColor(Color color);
+
+    Color GetTextColor();
+
+    void SetRawImage(Texture2D texture);
+
+    void SetImageColor(Color color);
+
+    float GetImageFillAmount();
+
+    void PlayAnimation(string ani);
+
+    void SetMaskWidth(float width);
+
+    void SetInputFieldVal(string val);
+
+    string GetInputFieldVal();
+
+    void SetInputFieldChangeEvent(Action<string> onFieldChange);
+
+    void SetToggleChangeEvent(Action<bool> onToggleChange);
+
+    bool GetToggleValue();
+
+    void SetToggleValue(bool isOn);
+
+    void SetDropListItems(List<string> items);
+
+    void SetRectSize(Vector2 size);
+}
